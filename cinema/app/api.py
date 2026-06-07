903d3e046c4bdec216866a0ab9e2f1caee292c74
YellowTuxedo72 <yellowtuxedo72@gmail.com>
2026-06-08 00:17:45 +0900

    fast fix2

--- cinema/app/api.py
+++ cinema/app/api.py
@@ -16,22 +16,22 @@ api = NinjaAPI()
 def get_active_movies(request):
     return MovieService.get_active_movies(request)
 
-
+#Эндпоинт для выгрузки списка сеансов
 @api.get("/sessions/schedule", response={200: DayScheduleOut, 400: dict})
 def get_schedule(request, filters: Query[ScheduleFilterParams]):
     return ScheduleService.get_schedule(request, filters)
 
-
+#Эндпоинт для выгрузки схемы зала конкретного сеанса
 @api.get("/sessions/{session_id}/map", response={200: HallMapOut, 400: dict})
 def get_hall_map(request, session_id: int):
     return HallService.get_hall_map(request, session_id)
 
-
+#Эндпоинт создание билетов со статусом бронированных мест
 @api.post("/tickets/book/", response={201: OrderCreatedOut, 400: dict})
-def book_tickets(request, data: BookTicketsInput):
-    return BookingService.book(data)
+def book_tickets(request, payload: BookTicketsInput):
+    return BookingService.book(payload)
 
-
+#Эндпоинт симуляции оплаты + смена статуса билетов на оплаченные
 @api.post("/payment/mock/{tickets_str}/pay/",response={200: dict, 400: dict})
 def fake_pay(request, tickets_str: str):
     return PaymentService.pay(tickets_str)
